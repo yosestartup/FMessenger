588d844406609d63aa41a0eb69660e929643d889
Oleksandr Bambuliak <yosestartup@gmail.com>
2018-03-05 17:01:47 +0100

    Back

---
+++ FirebaseTest/Controller/UserCell.swift
@@ -0,0 +1,13 @@
+//
+//  UserCell.swift
+//  FirebaseTest
+//
+//  Created by Александр on 03.03.18.
+//  Copyright © 2018 hilton. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: NSObject {
+
+}
